aa382adc404a64a6eb4cf68e2f0213e84d2e772a
yanaph <fan37yana@gmail.com>
2022-09-28 09:15:54 +0200

    module1

--- src/main/java/ua/fan/Task1.java
+++ src/main/java/ua/fan/Task1.java
@@ -1,4 +1,37 @@
 package ua.fan;
 
+import java.util.Arrays;
+
 public class Task1 {
+    public static void main(String[] args) {
+        int[] initialArray = {1, 4, 5, 1, 1, 3};
+        int[] sortedArray = bubbleSort(initialArray);
+        System.out.println("Initial: " + Arrays.toString(initialArray));
+        System.out.printf("Quantity of unique elements: %d%n", countUniqueElements(sortedArray));
+    }
+
+    private static int countUniqueElements(int[] array) {
+        int count = 1;
+        for (int i = 0; i < array.length - 1; i++) {
+            if (array[i] != array[i + 1]) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    private static int[] bubbleSort(int[] initialArray) {
+        int[] sorted = Arrays.copyOf(initialArray, initialArray.length);
+        int tmp;
+        for (int i = 0; i < sorted.length; i++) {
+            for (int j = 1; j < sorted.length - i; j++) {
+                if (sorted[j - 1] > sorted[j]) {
+                    tmp = sorted[j - 1];
+                    sorted[j - 1] = sorted[j];
+                    sorted[j] = tmp;
+                }
+            }
+        }
+        return sorted;
+    }
 }
--- src/main/java/ua/fan/Task2.java
+++ src/main/java/ua/fan/Task2.java
@@ -1,4 +1,28 @@
 package ua.fan;
 
+import java.util.Scanner;
+
 public class Task2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter current coordinates: ");
+        String current = scanner.nextLine();
+        System.out.print("Enter destination coordinates: ");
+        String destination = scanner.nextLine();
+        System.out.println("Possibility of this movement: " + isPossibleMovement(current, destination));
+        scanner.close();
+    }
+
+    private static boolean isPossibleMovement(String current, String destination) {
+        boolean isPossible = false;
+        char[] currentPosition = current.toUpperCase().toCharArray();
+        char[] destinationPosition = destination.toUpperCase().toCharArray();
+        int letter = Math.abs(currentPosition[0] - destinationPosition[0]);
+        int digit = Math.abs(currentPosition[1] - destinationPosition[1]);
+
+        if ((letter == 2 && digit == 1) || (letter == 1 && digit == 2)) {
+            isPossible = true;
+        }
+        return isPossible;
+    }
 }
